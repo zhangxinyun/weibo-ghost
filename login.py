4f280aae3a6d7375bf4722a07d4bce789a40f4b1
Zhen Li <anthony@rmbp.local>
2015-12-08 21:05:14 +0800

    添加 view port

--- login.py
+++ login.py
@@ -2,7 +2,8 @@
 from ghost import Ghost
 
 ghost = Ghost(defaults={\
-    "user_agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"\
+    "user_agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36",\
+    "viewport_size": (1280, 950)
     })
 
 with ghost.start() as session:
