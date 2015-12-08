9d49ab4f2e667bacafd02f95c1c463ea03826c79
Zhen Li <anthony@rmbp.local>
2015-12-08 20:55:05 +0800

    更新 user agent

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 from ghost import Ghost
 
 ghost = Ghost(defaults={\
-    user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"\
+    "user_agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"\
     })
 
 with ghost.start() as session:
