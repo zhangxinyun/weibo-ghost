e2923511988b622f02e798341c656f9822de4c68
Zhen Li <anthony@rmbp.local>
2015-12-08 20:27:13 +0800

    修改 user_agent

--- login.py
+++ login.py
@@ -1,7 +1,9 @@
 #encoding:utf-8
 from ghost import Ghost
 
-ghost = Ghost()
+ghost = Ghost(defaults={\
+    user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"\
+    })
 
 with ghost.start() as session:
     page, extra_resources = session.open("http://weibo.com/")
